b2b1e0b60fb3a7e25416b9971cfc509b564ccae4
adipurnama83 <adi.purnama1983@gmail.com>
2021-06-23 06:23:07 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Jika Bot Ikan tidak jalan silahkan Ganti Token Ikan
+$tokenIkan = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMzU2NTMyMzkifQ.eQfjQLO5ZisUgZvkhA38eGK_Xvyn_RtxAbeO0";
+
+
+$token = "xxx";
+$userId = "xxx";
+$uuid = "xxx";
+$userAgent = "Mozilla/5.0 (Linux; Android 10; Redmi Note 8 Pro Build/QP1A.190711.020; wv) AppleWebKit/";
